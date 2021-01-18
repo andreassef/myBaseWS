5b813524ac0792cf97e11ee4e8275104ee49e817
Andre Assef <andreassef02@gmail.com>
2021-01-18 00:07:41 -0400

    [NEW] - Novo modulo de um modelo do sequelize-users

---
+++ src/db/models/user.js
@@ -0,0 +1,30 @@
+'use strict';
+const { v4: uuidv4 } = require('uuid')
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  User.init({
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    cpf: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  
+  User.beforeCreate((User, options) => {
+    return User.id = uuidv4()
+  })
+  return User;
+};
